68a85c1f063892e30c614d4873b8e767646e682d
Paulo Mário Valente Bumba <143729787+PauloBumba@users.noreply.github.com>
2024-08-16 21:46:09 -0300

    Configurado o EF

---
+++ Atividades/Aula_Entity_Framework/Aula_Entity_Framework/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Aula_Entity_Framework.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
