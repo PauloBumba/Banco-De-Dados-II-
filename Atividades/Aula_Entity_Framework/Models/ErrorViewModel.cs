0821ee768dd0811b3c1e54c784d5f7a794f99bfb
Paulo Mário Valente Bumba <143729787+PauloBumba@users.noreply.github.com>
2024-09-13 19:28:00 -0300

    Update

--- Atividades/Aula_Entity_Framework/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Aula_Entity_Framework.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- Atividades/Aula_Entity_Framework/Models/Person.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Aula_Entity_Framework.Models
-{
-    public class Person
-    {
-        public long Id { get; set; }
-        public string? Name { get; set; }
-        public DateTime BirthDate { get; set; }
-    }
-}
